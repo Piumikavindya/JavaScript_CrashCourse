297b85f3af156031787f2a370535235c08ff2e1b
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-18 11:06:21 +0530

    Add basic Datatypes and Variable commands

---
+++ Basic.js
@@ -0,0 +1,13 @@
+
+//Data Types => String, Number, Boolean, Object,
+
+
+// Variables and 
+
+//String
+var userName = "ppk";
+console.log("User Name:"+ userName);
+
+//Number
+var age = 25;
+console.log("Age:"+ age);
