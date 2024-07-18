87b9004e5c2d61b1cbd740f617ada1c2bd43edce
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-18 12:06:21 +0530

    complete function section

--- Js-app/app.js
+++ Js-app/app.js
@@ -1,5 +1,25 @@
 
 let userName = "piumi"  //value can be chage dynamically
-console.log(userName);
+console.log(`User Name is ${userName}`);
 
 
+// functions
+
+function calculateShippingCost(){
+
+    let totalPrice = 19;
+    let shippingCost;
+
+    if(totalPrice <= 10){
+        shippingCost = 5;
+    } else if(totalPrice  <=20){
+        shippingCost = 3;
+    }else{
+        shippingCost = 0;
+    }
+
+    console.log(`Shipping Cost is ${shippingCost}`);
+}
+
+// calling the function
+calculateShippingCost();
