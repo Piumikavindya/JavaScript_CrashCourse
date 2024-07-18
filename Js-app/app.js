cf00017faa1454d4250e59c7440e0c5e17e9c93c
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-18 12:24:24 +0530

    complete crash curse

--- Js-app/app.js
+++ Js-app/app.js
@@ -45,3 +45,36 @@ function calculateShippingCost1(totalPriceParam){
 
 // calling the function
 calculateShippingCost1(totalPrice1 = 19);
+
+
+// Using const 
+// these values can't change programmatically.. in here we can update values only
+const standardShippingCost = 5;
+const discountedShippingCost = 3;
+
+function calculateShippingCost2(totalPriceParam){
+
+
+    let shippingCost;
+
+    if(totalPriceParam <= 10){
+        shippingCost = standardShippingCost;
+    } else if(totalPriceParam  <=20){
+        shippingCost = standardShippingCost;
+    }else{
+        shippingCost = 0;
+    }
+
+    console.log(`Shipping Cost is ${shippingCost}`);
+    console.log(`for total price of ${totalPriceParam}`);
+    console.log("****************************")
+}
+
+// user starts shopping
+calculateShippingCost2(totalPrice = 10);
+
+// user continues shopping
+calculateShippingCost2(totalPrice = 19);
+
+// add another items
+calculateShippingCost2(totalPrice = 25);
