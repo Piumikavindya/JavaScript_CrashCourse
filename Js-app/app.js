3f6bd4acb85ab220e1f92706009f87205ce0a65b
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-18 12:13:00 +0530

    complete functions with paramerters

--- Js-app/app.js
+++ Js-app/app.js
@@ -4,7 +4,7 @@ console.log(`User Name is ${userName}`);
 
 
 // functions
-
+// fuctions without parameter assigning
 function calculateShippingCost(){
 
     let totalPrice = 19;
@@ -23,3 +23,25 @@ function calculateShippingCost(){
 
 // calling the function
 calculateShippingCost();
+
+
+// fuctions with parameter assigning
+function calculateShippingCost1(totalPriceParam){
+
+
+    let shippingCost;
+
+    if(totalPriceParam <= 10){
+        shippingCost = 5;
+    } else if(totalPriceParam  <=20){
+        shippingCost = 3;
+    }else{
+        shippingCost = 0;
+    }
+
+    console.log(`Shipping Cost is ${shippingCost}`);
+    console.log(`for total price of ${totalPriceParam}`);
+}
+
+// calling the function
+calculateShippingCost1(totalPrice1 = 19);
