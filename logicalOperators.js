b70c7f1d34e0fc0f9c439d80f045f554008ca783
Piumi Kavindya <piyumikavindyappk@gmail.com>
2024-07-18 11:54:45 +0530

    complete logical operatiors

---
+++ logicalOperators.js
@@ -0,0 +1,31 @@
+
+// AND -> &&
+ let todayDate;
+ let birthdayDate;
+ let birthdayDisplyed;
+
+ if (today === birthdayDate && birthdayDisplyed === true){
+    //show notification
+ } else{
+        //do nothing
+ }
+
+
+ // OR -> ||
+ let totalPrice;
+ let amazonePrize;
+
+ if (totalPrice > 20 || amazonePrize === true){
+    //free shipment
+ }else{
+    // no free shipment
+ }
+
+
+ //NOR -> !
+ if (!(totalPrice > 20 || amazonePrize === true)){
+    //free shipment
+ }else{
+    // no free shipment
+ }
+
